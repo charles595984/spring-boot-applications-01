0da446cf89c2129d82d891b5d4a8eeeee86603c7
charles <charles595984@gmail.com>
2026-02-18 11:34:28 +0530

    Delete spring-boot-cache directory

--- spring-boot-cache/src/main/java/com/skcode/springbootcache/repo/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.skcode.springbootcache.repo;
-
-import com.skcode.springbootcache.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
