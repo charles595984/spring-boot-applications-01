8a6c9e8a809d37cb7e443c5b323a8d278dd0fc2f
charles <charles595984@gmail.com>
2026-02-23 19:22:42 +0530

    Delete auth-security-service directory

--- auth-security-service/src/main/java/com/skcode/authsecurityservice/exception/UserNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.skcode.authsecurityservice.exception;
-
-public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
-
--- auth-security-service/src/main/java/com/skcode/authsecurityservice/repo/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.skcode.authsecurityservice.repo;
-
-import com.skcode.authsecurityservice.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
