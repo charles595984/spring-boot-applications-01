7f6d8971cf04c60ccd12c9d51becb9f68ac39289
charles <charles595984@gmail.com>
2026-02-23 20:53:43 +0530

    Delete Java_project_01 directory

--- Java_project_01/src/com/skcoder/interviewpoints/singleton/syncronized/volatiles/SingletonMain.java
+++
@@ -1,9 +0,0 @@
-package com.skcoder.interviewpoints.singleton.syncronized.volatiles;
-
-public class SingletonMain {
-    void main() {
-        DatabaseConnection conn01 = DatabaseConnection.getInstacne();
-        DatabaseConnection conn02 = DatabaseConnection.getInstacne();
-        System.out.println(conn01 == conn02);
-    }
-}
