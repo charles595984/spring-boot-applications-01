652fda871fe64c4204a3b7d1ddcca508156cc9c2
charles <charles595984@gmail.com>
2026-02-23 17:53:29 +0530

    Add files via upload

---
+++ auth-security-service/src/test/java/com/skcode/authsecurityservice/AuthSecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skcode.authsecurityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSecurityServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
